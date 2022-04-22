d81c9ffe7a6d0ca1c8dd5971ab6091d5b28e1d44
Léo Carvalho <carvalho.csleo@gmail.com>
2022-04-22 15:02:42 -0300

    Cria técnica de Code Splitting

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import 'bootstrap-icons/font/bootstrap-icons.css'
 import 'bootstrap/dist/css/bootstrap.css';
-import {BrowserRouter} from 'react-router-dom';
-import { useContext, useState, useEffect } from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import { useContext, useState, useEffect, lazy, Suspense } from 'react';
 
 import { DarkModeContext } from './contexts/DarkModeContext';
 
-import Navbar from './components/Navbar';
-
 import Router from './routes';
+
+// import Navbar from './components/Navbar';
+
+const Navbar = lazy(() => import('./components/Navbar'));
 
 function App() {
   const [classeModoEscuro, setClasseModoEscuro] = useState('');
-  const {modoEscuro} = useContext(DarkModeContext);
+  const { modoEscuro } = useContext(DarkModeContext);
 
   useEffect(() => {
     setClasseModoEscuro(modoEscuro)
@@ -20,8 +22,10 @@ function App() {
   return (
     <div className={classeModoEscuro}>
       <BrowserRouter>
-        <Navbar />
-        <Router />
+        <Suspense fallback={<div>Carregando...</div>}>
+          <Navbar />
+          <Router />
+        </Suspense>
       </BrowserRouter>
     </div>
   );
--- src/routes.js
+++ src/routes.js
@@ -1,23 +1,31 @@
 import { Routes, Route } from 'react-router-dom';
+import { lazy, Suspense } from 'react';
 
-import Error404 from './pages/Errors/Error404';
-import Disabled from './pages/Errors/Disabled';
+// import Error404 from './pages/Errors/Error404';
+// import Disabled from './pages/Errors/Disabled';
 
-import Perfil from './pages/Perfil';
-import PesquisaUsuario from './pages/PesquisaUsuario';
+// import Perfil from './pages/Perfil';
+// import PesquisaUsuario from './pages/PesquisaUsuario';
+
+const Error404 = lazy(() => import('./pages/Errors/Error404'));
+const Disabled = lazy(() => import('./pages/Errors/Disabled'))
+const Perfil = lazy(() => import('./pages/Perfil'))
+const PesquisaUsuario = lazy(() => import('./pages/PesquisaUsuario'));
 
 function Router() {
   return (
     <div className="container-lg">
-      <Routes>
-        <Route path='/perfil' element={ <Perfil />} />
-        <Route path='/perfil/:usuario' element={ <Perfil />} />
-        
-        <Route path='/pesquisa' element={ <PesquisaUsuario /> }/>
+      <Suspense fallback={<div>Carregando...</div>}>
+        <Routes>
+          <Route path='/perfil' element={<Perfil />} />
+          <Route path='/perfil/:usuario' element={<Perfil />} />
 
-        <Route path='/contact' element={<Disabled />} />
-        <Route path='*' element={<Error404 />} />
-      </Routes>
+          <Route path='/pesquisa' element={<PesquisaUsuario />} />
+
+          <Route path='/contact' element={<Disabled />} />
+          <Route path='*' element={<Error404 />} />
+        </Routes>
+      </Suspense>
     </div>
   );
 }
